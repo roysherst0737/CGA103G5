3bb20bf374c645f8c7bd9ed201711f4e628d7f54
Arick Yu <YuArick@gmail.com>
2022-08-19 14:18:27 +0800

    pub version2

---
+++ src/main/java/com/pub/controller/pubServlet2.java
@@ -0,0 +1,20 @@
+package com.pub.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/pubServlet2")
+public class pubServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}

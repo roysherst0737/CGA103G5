e58e167f4804cee6e4cbf608133366a491624125
Roy Sher <roysherst@gmail.com>
2022-08-19 17:39:23 +0800

    update master

--- src/main/java/com/util/Common.java
+++ src/main/java/com/util/Common.java
@@ -3,7 +3,7 @@ package com.util;
 public class Common {
 	
 	public static final String JNDIURL = "java:comp/env/jdbc/DBPool";
-	public static final String URL = "jdbc:mysql://localhost:3306/lonelybar?serverTimezone=Asia/Taipei";
+	public static final String URL = "jdbc:mysql://localhost:3306/lonelybar?useUnicode=yes&characterEncoding=utf8&useSSL=true&serverTimezone=Asia/Taipei";
 	public static final String USERNAME = "cga10305";
 	public static final String PASSWORD = "123qweqwe";
 	public static final String DRIVER = "com.mysql.cj.jdbc.Driver";

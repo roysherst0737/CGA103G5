d548a82fd584f1991d5506d82798ff5ac0e4e594
Roy Sher <roysherst@gmail.com>
2022-08-27 17:34:29 +0800

    update 0827

--- src/main/java/com/pub/model/Pub_DAO_H.java
+++
@@ -1,6 +0,0 @@
-package com.pub.model;
-
-import com.pub.service.CoreDao;
-
-public interface Pub_DAO_H extends CoreDao<Pub, Integer> {
-}
--- src/main/java/com/pub/service/PubConstants.java
+++
@@ -1,8 +0,0 @@
-package com.pub.service;
-
-import com.pub.model.pub_Service_H;
-import com.pub.model.pub_Service_H_impl;
-
-public class PubConstants {
-	public static final pub_Service_H SERVICE = new pub_Service_H_impl();
-}

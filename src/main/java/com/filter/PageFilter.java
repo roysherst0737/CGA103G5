07d770a3834e0cca989b9e3b14d5f2e1ec1a9137
Arick Yu <YuArick@gmail.com>
2022-08-28 16:26:46 +0800

    test filter

--- src/main/java/com/filter/PageFilter.java
+++ src/main/java/com/filter/PageFilter.java
@@ -1,28 +1,30 @@
-//package com.filter;
-//
-//import static com.util.Constants.PREFIX_WEB_INF;
-//
-//import java.io.IOException;
-//
-//import javax.servlet.FilterChain;
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebFilter;
-//import javax.servlet.http.HttpFilter;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//@WebFilter("/*")
-//public class PageFilter extends HttpFilter {
-//	private static final long serialVersionUID = 1L;
-//
-//	@Override
-//	protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-//		final String requestPath = request.getServletPath();
-//		if (!requestPath.contains(PREFIX_WEB_INF)
-//				&& (requestPath.endsWith(".html") || requestPath.endsWith(".css") || requestPath.endsWith(".js"))) {
-//			request.getRequestDispatcher(PREFIX_WEB_INF + requestPath).forward(request, response);
-//		} else {
-//			chain.doFilter(request, response);
-//		}
-//	}
-//}
+package com.filter;
+
+import static com.util.Constants.PREFIX_WEB_INF;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter(filterName = "PageFilter", urlPatterns = "/front-end/pub/*")
+public class PageFilter extends HttpFilter {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+		final String requestPath = request.getServletPath();
+		System.out.println(requestPath);
+		System.out.println(request.getContextPath());
+		if (!requestPath.contains(request.getContextPath())
+				&& (requestPath.endsWith(".html") || requestPath.endsWith(".css") || requestPath.endsWith(".js"))) {
+			request.getRequestDispatcher("/front-end").forward(request, response);
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+}

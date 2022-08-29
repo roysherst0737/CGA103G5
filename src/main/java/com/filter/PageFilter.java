c228bccd3f2d38b88661ea3d7ffbdd5200b505c3
Arick Yu <YuArick@gmail.com>
2022-08-29 14:45:00 +0800

    123

--- src/main/java/com/filter/PageFilter.java
+++ src/main/java/com/filter/PageFilter.java
@@ -3,6 +3,7 @@ package com.filter;
 
 import java.io.IOException;
 
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
@@ -10,18 +11,22 @@ import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//@WebFilter(filterName = "PageFilter", urlPatterns = "/front-end/pub/*")
+@WebFilter(filterName = "PageFilter", urlPatterns = "/*")
 public class PageFilter extends HttpFilter {
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+//		let path = "http://";
+//		let hostname = window.location.host;
+//		let pathname = window.location.pathname.substring(0, window.location.pathname.indexOf('/', 2));
+//		let script = document.createElement('script')
+		
+		
 		final String requestPath = request.getServletPath();
-		System.out.println(requestPath);
-		System.out.println(request.getContextPath());
 		if (!requestPath.contains(request.getContextPath())
 				&& (requestPath.endsWith(".html") || requestPath.endsWith(".css") || requestPath.endsWith(".js"))) {
-			request.getRequestDispatcher("/front-end").forward(request, response);
+			request.getRequestDispatcher(requestPath).forward(request, response);
 		} else {
 			chain.doFilter(request, response);
 		}

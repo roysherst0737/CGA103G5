da9dc16c4cd3cb0b0b2c700909875510cad9fcfc
Roy Sher <roysherst@gmail.com>
2022-09-15 16:05:06 +0800

    訂單進度30%

--- src/main/java/com/order_detail/model/Order_detail_DAO.java
+++ src/main/java/com/order_detail/model/Order_detail_DAO.java
@@ -6,7 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
